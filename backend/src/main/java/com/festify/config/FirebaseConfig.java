3a9bbcfbb13716891e4013c7eec6d98b4b768d33
Matthew Kwon <matthew.o.kwon@gmail.com>
2024-05-22 16:16:39 -0700

    Hid some sensitive information

--- backend/src/main/java/com/festify/config/FirebaseConfig.java
+++ backend/src/main/java/com/festify/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package com.festify.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -12,11 +13,13 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.service.account.path}")
+    private String serviceAccountPath;
+
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+            FileInputStream serviceAccount = new FileInputStream(serviceAccountPath);
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
